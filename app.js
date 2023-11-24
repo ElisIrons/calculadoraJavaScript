3c18582cb7af3f01a6b54af795e116f4fbbb6e9c
Elis <86434261+ElisIrons@users.noreply.github.com>
2023-11-24 13:56:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
-'use strict'
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+  const switcher = document.querySelector(".btn-modo");
+  const body = document.body;
 
-const switcher = document.querySelector('.btn-modo');
+  switcher.addEventListener("click", function () {
+    body.classList.toggle("dark-theme");
 
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
-
-    var className = document.body.className;
-    if (className == "light-theme") {
-        this.textContent = "🌗"; 
+    if (body.classList.contains("dark-theme")) {
+      this.textContent = "🌞";
+      body.style.background = "#2E2E2E";
+    } else {
+      this.textContent = "🌚";
+      body.style.background = "#c5c5c5";
     }
-    else {
-        this.textContent = "🌗";
-    }
+  });
+
+  console.log("current class name: " + body.className);
 });
-
-console.log( 'current class name: ' + className);
-
 function calcular(tipo, valor) {
-    console.log(tipo, valor)
+  console.log(tipo, valor);
 
-    if(tipo === 'acao') {
-
-      if(valor === 'c'){
-        document.getElementById('resultado').value = ''
-      }
-
-      if( valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
-        document.getElementById('resultado').value += valor
-    }  
-    if(valor === '=' ) {
-      var valor_campo = eval(document.getElementById('resultado').value)
-      document.getElementById('resultado').value = valor_campo
+  if (tipo === "acao") {
+    if (valor === "c") {
+      document.getElementById("resultado").value = "";
     }
+
+    if (
+      valor === "+" ||
+      valor === "-" ||
+      valor === "*" ||
+      valor === "/" ||
+      valor === "."
+    ) {
+      document.getElementById("resultado").value += valor;
+    }
+    if (valor === "=") {
+      var valor_campo = eval(document.getElementById("resultado").value);
+      document.getElementById("resultado").value = valor_campo;
+    }
+  } else if (tipo === "valor") {
+    document.getElementById("resultado").value += valor;
   }
-  else if (tipo === 'valor') {
-      document.getElementById('resultado').value +=  valor
-    }
-
-  }    
+}
